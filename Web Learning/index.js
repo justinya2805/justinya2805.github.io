809f7cc3a07a8ae3f76263a724d0d9b665fa48d7
Justinya <tuandat277353@gmail.com>
2025-01-16 15:23:49 +0700

    Add files via upload
    First try

---
+++ Web Learning/index.js
@@ -0,0 +1,24 @@
+let name = "Justinya";
+console.log(name);
+
+//Rule to naming variables
+//can't be a keyword
+//meaningful name
+//can't start with a number
+//can't contain space or special characters
+//case-sensitive
+
+let Ten = "Tuan Dat";
+let Ho = "Le";
+const interestRate = 0.3;
+console.log(interestRate);
+
+
+//value types
+let myName = "Tuan Dat"; //string literal
+let age = 19; //number literal
+let isGraduated = false; //boolean literal
+let lastName = undefined;
+let salary = null;
+console.log("Ho va Ten : ", myName);
+
